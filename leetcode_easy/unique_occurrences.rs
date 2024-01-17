da9dfa1b569dcd11c07e78fd1a74a6429da468fb
Saurabh Shinde <37367457+saurabh10041998@users.noreply.github.com>
2024-01-17 11:50:15 +0530

    Leetcode solution to the problem 'unique-number-of-occurrences': functional programming style

--- leetcode_easy/unique_occurrences.rs
+++ leetcode_easy/unique_occurrences.rs
@@ -1,16 +1,18 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn unique_occurrences(arr: Vec<i32>) -> bool {
-    let mut mp = HashMap::new();
-    for c in arr {
-        mp.entry(c).and_modify(|v| *v += 1).or_insert(1);
-    }
-    let mut hs = HashSet::new();
-    for (_k, v) in mp.iter() {
-        hs.insert(*v);
-    }
-    hs.len() == mp.len()
+    let freq = arr.into_iter().fold(HashMap::new(), |mut accum, n| {
+        accum
+            .entry(n)
+            .and_modify(|f: &mut usize| *f += 1)
+            .or_insert(1usize);
+        accum
+    });
+    let hs = freq.iter().fold(HashSet::new(), |mut accum, (&_k, &v)| {
+        accum.insert(v);
+        accum
+    });
+    freq.len() == hs.len()
 }
 
 #[cfg(test)]
@@ -19,24 +21,21 @@ pub mod tests {
     #[test]
     fn run_tc1() {
         let arr = vec![1, 2, 2, 1, 1, 3];
-        assert_eq!(unique_occurrences(arr), true);
+        assert!(unique_occurrences(arr));
     }
-
     #[test]
     fn run_tc2() {
         let arr = vec![1, 2];
-        assert_eq!(unique_occurrences(arr), false);
+        assert!(!unique_occurrences(arr));
     }
-
     #[test]
     fn run_tc3() {
         let arr = vec![-3, 0, 1, -3, 1, 1, 1, -3, 10, 0];
-        assert_eq!(unique_occurrences(arr), true);
+        assert!(unique_occurrences(arr));
     }
 }
 
 fn main() {
-    //println!("Hello, world!");
-    let arr = vec![1, 2, 2, 1, 1, 3];
-    assert_eq!(unique_occurrences(arr), true);
+    let arr = vec![-3, 0, 1, -3, 1, 1, 1, -3, 10, 0];
+    assert!(unique_occurrences(arr));
 }
