7ef7196649aa8c301f39da32909e30bf873c8f7a
saurabh10041998 <shindesaurabhpict@gmail.com>
2024-03-22 07:04:38 +0000

    Leetcode solution to the problem 'palindrome-linked-list'

--- leetcode_easy/palindrome_linked_list.rs
+++ leetcode_easy/palindrome_linked_list.rs
@@ -1,23 +1,18 @@
-use std::mem;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ListNode {
+#[derive(Debug)]
+struct ListNode {
     val: i32,
-    next: Option<Box<ListNode>>
+    next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
     fn new(val: i32) -> Self {
-        ListNode {
-            val,
-            next: None,
-        }
+        ListNode { val, next: None }
     }
 }
 
-fn build_list(buffer: Vec<i32>) -> Option<Box<ListNode>> {
+fn build_lst(buffer: Vec<i32>) -> Option<Box<ListNode>> {
     let mut head = None;
-    for &ele in buffer.iter().rev() {
+    for ele in buffer.into_iter().rev() {
         let mut new_node = Box::new(ListNode::new(ele));
         new_node.next = head.take();
         head = Some(new_node);
@@ -25,47 +20,42 @@ fn build_list(buffer: Vec<i32>) -> Option<Box<ListNode>> {
     head
 }
 
-fn count_nodes(head: &Option<Box<ListNode>>) -> i32 {
-    let mut count = 0;
-    let mut curr = head;
-    while let Some(node) = curr {
-        count += 1;
-        curr = &node.next;
+fn count(mut head: Option<&Box<ListNode>>) -> usize {
+    let mut cnt = 0;
+    while let Some(ref node) = head {
+        cnt += 1;
+        head = node.next.as_ref();
     }
-    count
+    cnt
 }
 
-fn partition_list(mut head: Option<Box<ListNode>>) -> (Option<Box<ListNode>>, Option<Box<ListNode>>) {
+fn partition_lst(
+    mut head: Option<Box<ListNode>>,
+) -> (Option<Box<ListNode>>, Option<Box<ListNode>>) {
+    let cnt = count(head.as_ref());
+    let mid = cnt / 2;
     let mut top = None;
-    let mid = count_nodes(&head) / 2;
     for _ in 0..mid {
         let mut node = head.take().unwrap();
-        head = mem::replace(&mut node.next, top.take());
+        head = std::mem::replace(&mut node.next, top.take());
         top = Some(node);
     }
     (top, head)
 }
 
 fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
-    let head = head;
-    let mut is_odd = false;
-    let n = count_nodes(&head);
-    if n == 1 {
-        return true;
-    }
-    if n % 2 != 0 {
-        is_odd = true;
-    }
-    let (top, bottom) = partition_list(head); 
-    let mut top_node_ref = &top;
+    let is_odd = count(head.as_ref()) % 2 == 1;
+    let (top, bottom) = partition_lst(head);
+    let mut top_node_ref = top.as_ref();
     let mut bottom_node_ref = if is_odd {
-        &bottom.as_ref().unwrap().next
-    }else {
-        &bottom
+        bottom.as_ref().unwrap().next.as_ref()
+    } else {
+        bottom.as_ref()
     };
+
     while let (Some(top_node), Some(bottom_node)) = (top_node_ref, bottom_node_ref) {
-        top_node_ref = &top_node.next;
-        bottom_node_ref = &bottom_node.next;
+        top_node_ref = top_node.next.as_ref();
+        bottom_node_ref = bottom_node.next.as_ref();
         if top_node.val != bottom_node.val {
             return false;
         }
@@ -74,25 +64,21 @@ fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
 }
 
 #[cfg(test)]
-pub mod tests  {
+pub mod tests {
     use super::*;
     #[test]
     fn run_tc1() {
-        let buffer = vec![1,2,2,1];
-        let head = build_list(buffer);
+        let head = build_lst(vec![1, 2, 2, 1]);
         assert_eq!(is_palindrome(head), true);
     }
-
     #[test]
     fn run_tc2() {
-        let buffer = vec![1,2];
-        let head = build_list(buffer);
+        let head = build_lst(vec![1, 2]);
         assert_eq!(is_palindrome(head), false);
     }
 }
 
 fn main() {
-    let buffer = vec![1,2,2,1];
-    let head = build_list(buffer);
+    let head = build_lst(vec![1, 2, 2, 1]);
     assert_eq!(is_palindrome(head), true);
 }
